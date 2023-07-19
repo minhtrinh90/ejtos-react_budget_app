24d8e73e0067f2e6a434483b8496f4db9846cbd7
Minh Hoang Trinh <trinhhoangminhbk@gmail.com>
2023-07-19 23:19:54 +0700

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,34 @@ import React, {useContext} from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = ()=> {
-    const {budget} = useContext(AppContext);
+    const {budget,currency} = useContext(AppContext);
+    const { dispatch} = useContext(AppContext);
+    //const [cost, setCost] = useState('');
+    // const setBudget = (budget1)=>(
+    //     budget = budget1
+    // )
+    // const expense = {
+    //     cost: parseInt(cost),
+    // };
+
     return (
         <div className='alert alert-secondary'>
-            <span> Budget: ${budget} </span>
+            <span> Budget: {currency} 
+            <input
+                required = 'required'
+                type = 'number'
+                id = 'budget'
+                value = {budget}
+                step = '10'
+                style = {{ marginLeft: '0 rem' , size: 10, width: '60px'}}
+                onChange = {(event) => dispatch({
+                        type: 'SET_BUDGET',
+                        payload: event.target.value,
+                        })
+                  }
+            >
+            </input></span>
+            
         </div>
     );
 };
