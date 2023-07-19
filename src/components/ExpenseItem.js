c3d9b6a9b61117c0e7ec51e0a3c102f4b15440e3
Minh Hoang Trinh <trinhhoangminhbk@gmail.com>
2023-07-20 02:38:00 +0700

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { IoIosAddCircleOutline } from 'react-icons/io';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
+import { IoMdRemoveCircle } from 'react-icons/io';
 import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
     const handleDeleteExpense = () => {
         dispatch({
             type: 'DELETE_EXPENSE',
@@ -19,12 +21,23 @@ const ExpenseItem = (props) => {
             payload: expense
         });
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td>{currency}{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}><IoIosAddCircleOutline size='1.5em'></IoIosAddCircleOutline></button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}><IoMdRemoveCircle size='1.5em'></IoMdRemoveCircle></button></td>
+        <td>< IoIosCloseCircleOutline size='1.5em' onClick={handleDeleteExpense}></IoIosCloseCircleOutline></td>
         </tr>
     );
 };
